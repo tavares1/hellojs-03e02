4ee2e5382c75a387cb3eea0d6cc0eadceda9d290
lucastavaresvasconcelos <lucastavaresvasconcelos@gmail.com>
2017-09-12 20:49:12 -0300

    adicionando a regra de data

--- monicoments.js
+++ monicoments.js
@@ -8,12 +8,17 @@ const api = axios.create({
 
 api.get("repos/seita-ifce/hello-js-v3/issues/2/comments").then(ret => {
     users = (ret.data)
+    //verifica se tem algum comentario
     if (users.length == 0){
         console.log("Nenhum comentário na issue!")
     }
-    for( let i = 0; i < users.length; i++){
-        if (users[i].user.login != "sombriks") {         
-            console.log(`Nome: ${users[i].user.login} - Comentário: ${users[i].body}`)
-        }
+    //percorre o array de usuarios, verifica se o login é sombriks e se a data é menor que 2017-09-14, utilizando template string.
+    for( let i = 0; i < users.length; i++){ 
+        let dataComentario = new Date(users[i].created_at)
+        if (users[i].user.login != "sombriks" && dataComentario < (new Date("2017-09-14"))) {
+                console.log(`---\nNome: ${users[i].user.login} \nComentário: ${users[i].body.trim()} \nData : ${dataComentario.getDate()+"/"+dataComentario.getMonth()+"/"+dataComentario.getFullYear()} \n---`)
+            }
     }
 })
+
+
