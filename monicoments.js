a7ce0fdb9138f1cf50293a0d200cbdbc039b808c
lucastavaresvasconcelos <lucastavaresvasconcelos@gmail.com>
2017-09-12 20:51:42 -0300

    ok é dia 15

--- monicoments.js
+++ monicoments.js
@@ -12,10 +12,10 @@ api.get("repos/seita-ifce/hello-js-v3/issues/2/comments").then(ret => {
     if (users.length == 0){
         console.log("Nenhum comentário na issue!")
     }
-    //percorre o array de usuarios, verifica se o login é sombriks e se a data é menor que 2017-09-14, utilizando template string.
+    //percorre o array de usuarios, verifica se o login é sombriks e se a data é menor que 2017-09-15, utilizando template string.
     for( let i = 0; i < users.length; i++){ 
         let dataComentario = new Date(users[i].created_at)
-        if (users[i].user.login != "sombriks" && dataComentario < (new Date("2017-09-14"))) {
+        if (users[i].user.login != "sombriks" && dataComentario < (new Date("2017-09-15"))) {
                 console.log(`---\nNome: ${users[i].user.login} \nComentário: ${users[i].body.trim()} \nData : ${dataComentario.getDate()+"/"+dataComentario.getMonth()+"/"+dataComentario.getFullYear()} \n---`)
             }
     }
