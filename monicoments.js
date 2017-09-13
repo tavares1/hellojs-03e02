7213e798410c878db0060990e4626817be319a15
lucastavaresvasconcelos <lucastavaresvasconcelos@gmail.com>
2017-09-12 21:31:04 -0300

    usando filter

--- monicoments.js
+++ monicoments.js
@@ -6,19 +6,24 @@ const api = axios.create({
         baseURL: "https://api.github.com/"
     })
 
-api.get("repos/seita-ifce/hello-js-v3/issues/2/comments").then(ret => {
-    users = (ret.data)
-    //verifica se tem algum comentario
-    if (users.length == 0){
-        console.log("Nenhum comentário na issue!")
-    }
-    //percorre o array de usuarios, verifica se o login é sombriks e se a data é menor que 2017-09-15, utilizando template string.
-    for( let i = 0; i < users.length; i++){ 
-        let dataComentario = new Date(users[i].created_at)
-        if (users[i].user.login != "sombriks" && dataComentario < (new Date("2017-09-15"))) {
-                console.log(`---\nNome: ${users[i].user.login} \nComentário: ${users[i].body.trim()} \nData : ${dataComentario.getDate()+"/"+dataComentario.getMonth()+"/"+dataComentario.getFullYear()} \n---`)
-            }
-    }
+api.get("repos/seita-ifce/hello-js-v3/issues/1/comments").then(ret => {
+    //ROOT JS
+    //users = (ret.data)
+    // //verifica se tem algum comentario
+    // if (users.length == 0){
+    //     console.log("Nenhum comentário na issue!")
+    // }
+    // //percorre o array de usuarios, verifica se o login é sombriks e se a data é menor que 2017-09-15, utilizando template string.
+    // for( let i = 0; i < users.length; i++){ 
+    //     let dataComentario = new Date(users[i].created_at)
+    //     if (users[i].user.login != "sombriks" && dataComentario < (new Date("2017-09-15"))) {
+    //             console.log(`---\nNome: ${users[i].user.login} \nComentário: ${users[i].body.trim()} \nData : ${dataComentario.getDate()+"/"+dataComentario.getMonth()+"/"+dataComentario.getFullYear()} \n---`)
+    //         }
+    // }
+    
+    //COOL JS
+    console.log(ret.data.filter(e=>e.user.login != "sombriks" && new Date(e.created_at) < new Date("2017-09-15")))
+
 })
 
 
